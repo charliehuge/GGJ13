8b01cb2f290e468b3023d6a1c6e2aa6ba0130e78
Alex Sink <alexsink@alexsink.org>
2013-01-27 02:45:18 -0800

    added a trigger block

--- Assets/Scripts/EnemyColliderPropigator.cs
+++ Assets/Scripts/EnemyColliderPropigator.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(BoxCollider))]
 public class EnemyColliderPropigator : MonoBehaviour 
 {
 	public Entity playerEntity;
